50a2a73af148ca7d2a2d359bdbe77abea58561cf
unknown <jinwook232@naver.com>
2025-04-08 14:51:04 +0900

    commit1

---
+++ Cprogramming/Lecture05/mian.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int, main()
+{
+	int i = 1, j = 2;
+	while (j < 7)
+	{
+		while (i)
+		{
+			printf("*");
+			i = i - 1;
+		}
+		printf("\n");
+		i = j;
+		j++;
+	}
+	return 0;
+}
